2bb947cf788311a0aff62bff869ea2927094bcc5
Soniya Patidar <soniyapatidar2016@gmail.com>
2022-12-03 14:32:42 +0000

    adding routes

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 const http = require("http");
 const port = 8081;
 
-const toDoList = ["Complete node byte","Play Cricket"];
+const toDoList = ["Complete node byte", "Play Cricket"];
 http
   .createServer((req, res) => {
     // callback function
-    const { method, url} = req;
-    console.log(method, url);
-    req.end();
+    const { method, url } = req;
+
+    if (url === "/todos") {
+      if (method === "GET") {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(toDoList.toString());
+      }
+    }else {
+      res.writeHead(501);
+    }
+
+    res.end();
   })
   .listen(port, () => {
     console.log(`Nodejs server started on port ${port}`);
